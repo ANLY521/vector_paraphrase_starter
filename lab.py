bb579b3cf526fa5d03fe93807a536049d668ace7
emmerkhofer <emm228@georgetown.edu>
2020-02-27 13:13:56 -0500

    lab solution

--- lab.py
+++ lab.py
@@ -1,4 +1,8 @@
 # coding: utf-8
+from gensim.models import KeyedVectors
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from scipy.stats import pearsonr
 from util import preprocess_text, parse_sts
 import argparse
 
@@ -6,30 +10,70 @@ import argparse
 def w2v_sentence_mean(sent, word2vec):
     """Creates a sentence representation by taking the mean of all in-vocabulary word vectors.
     Returns None if no words are in vocabulary."""
-    return None
+    toks = preprocess_text(sent)
+    veclist = [word2vec[tok] for tok in toks if tok in word2vec]
+    if len(veclist) == 0:
+        return None
+    mean_vec = np.mean(veclist, axis=0)
+    return mean_vec
 
 def w2v_sentence_product(sent, word2vec):
     """Creates a sentence representation by taking the mean of all in-vocabulary word vectors.
     Returns None if no words are in vocabulary."""
-    return None
+    toks = preprocess_text(sent)
+    veclist = [word2vec[tok] for tok in toks if tok in word2vec]
+    if len(veclist) == 0:
+        return None
+    prod_vec = np.product(veclist, axis=0)
+    return prod_vec
 
 
 def main(sts_dev, w2v_file):
     # load the texts
     dev_texts, dev_y = parse_sts(sts_dev)
 
-    # load word2vec using gensim KeyedVectors object
-    w2v_vecs = None
+    # load word2vec
+    w2v_vecs = KeyedVectors.load_word2vec_format(w2v_file, binary=False)
 
     # get cosine similarities of every pair in dev
     # if either sentence is completely out of vocabulary, record "0" as the similarity
     cos_sims_mean = []
-    cos_sims_product = []
 
-    pearson_mean = 0
+    # means first
+    for t1,t2 in dev_texts:
+        t1_vector_mean = w2v_sentence_mean(t1, w2v_vecs)
+        if t1_vector_mean is None:
+            cos_sims_mean.append(0)
+            continue
+        t1_vector_mean = t1_vector_mean.reshape((1, -1)) # shape for cosine similarity
+        t2_vector_mean = w2v_sentence_mean(t2, w2v_vecs)
+        if t2_vector_mean is None:
+            cos_sims_mean.append(0)
+            continue
+        t2_vector_mean = t2_vector_mean.reshape((1, -1))
+        pair_similarity = cosine_similarity(t1_vector_mean, t2_vector_mean)[0, 0]
+        cos_sims_mean.append(pair_similarity)
+
+    # now products
+    cos_sims_product = []
+    for t1, t2 in dev_texts:
+        t1_vector_product = w2v_sentence_product(t1, w2v_vecs)
+        if t1_vector_product is None:
+            cos_sims_product.append(0)
+            continue
+        t1_vector_product = t1_vector_product.reshape((1, -1)) # shape for cosine similarity
+        t2_vector_product = w2v_sentence_product(t2, w2v_vecs)
+        if t2_vector_product is None:
+            cos_sims_product.append(0)
+            continue
+        t2_vector_product = t2_vector_product.reshape((1, -1))
+        pair_similarity = cosine_similarity(t1_vector_product, t2_vector_product)[0, 0]
+        cos_sims_product.append(pair_similarity)
+
+    pearson_mean = pearsonr(cos_sims_mean, dev_y)
     print(f"word2vec mean pearsons: r={pearson_mean[0]:.03}")
 
-    pearson_prod = 0
+    pearson_prod = pearsonr(cos_sims_product, dev_y)
     print(f"word2vec product pearsons: r={pearson_prod[0]:.03}")
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -8,7 +8,14 @@ def preprocess_text(text, stem=False):
     """Preprocess one sentence: tokenizes, lowercases, applies the Porter stemmer,
      removes punctuation tokens and stopwords.
      Returns a list of strings."""
-    return []
+    stops = set(stopwords.words('english'))
+    toks = word_tokenize(text)
+    if stem:
+        stemmer = PorterStemmer()
+        toks = [stemmer.stem(tok) for tok in toks]
+    toks_nopunc = [tok for tok in toks if tok not in string.punctuation]
+    toks_nostop = [tok for tok in toks_nopunc if tok not in stops]
+    return toks_nostop
 
 # TODO: lab, homework
 def parse_sts(data_file):
@@ -20,9 +27,15 @@ def parse_sts(data_file):
     texts = []
     labels = []
 
+    with open(data_file, 'r') as dd:
+        for line in dd:
+            fields = line.strip().split("\t")
+            labels.append(float(fields[4]))
+            t1 = fields[5].lower()
+            t2 = fields[6].lower()
+            texts.append((t1, t2))
     return texts, labels
 
-# TODO: lab, homework
 def sts_to_pi(texts, sts_labels, max_nonparaphrase, min_paraphrase):
     """Convert a dataset from semantic textual similarity to paraphrase.
     Remove any examples that are > max_nonparaphrase and < min_paraphrase."""
