1d1b9736826ebb372bf6a28d9d89213cc751486b
emmerkhofer <emm228@georgetown.edu>
2021-03-16 21:15:58 -0400

    updates for 2021

--- lab.py
+++ lab.py
@@ -6,26 +6,32 @@ import argparse
 def w2v_sentence_mean(sent, word2vec):
     """Creates a sentence representation by taking the mean of all in-vocabulary word vectors.
     Returns None if no words are in vocabulary."""
+    tokens = preprocess_text(sent)
     return None
 
 def w2v_sentence_product(sent, word2vec):
     """Creates a sentence representation by taking the mean of all in-vocabulary word vectors.
     Returns None if no words are in vocabulary."""
+    tokens = preprocess_text(sent)
     return None
 
 
 def main(sts_dev, w2v_file):
-    # load the texts
+
+    # TODO 1: load the texts
     dev_texts, dev_y = parse_sts(sts_dev)
 
-    # load word2vec using gensim KeyedVectors object
+    # TODO 2: load word2vec using gensim KeyedVectors object
     w2v_vecs = None
 
-    # get cosine similarities of every pair in dev
-    # if either sentence is completely out of vocabulary, record "0" as the similarity
+    # TODO 3: Define the functions above that compose word representations into sentence representations
+
+    # TODO 4: get cosine similarities of every sentence pair in dev
+    # if either sentence is completely out of vocabulary, record "0" as the similarity for the pair
     cos_sims_mean = []
     cos_sims_product = []
 
+    # TODO 5: Measure correlation with STS labels for the two ways of computing word2vec sentence representations
     pearson_mean = 0
     print(f"word2vec mean pearsons: r={pearson_mean[0]:.03}")
 
